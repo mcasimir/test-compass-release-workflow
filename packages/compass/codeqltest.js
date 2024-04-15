f9f65eb822d103b1162ff9eadd68ea4407738b86
Maurizio Casimirri <maurizio.cas@gmail.com>
2024-04-15 12:24:13 +0200

    Create codeqltest.js

---
+++ packages/compass/codeqltest.js
@@ -0,0 +1,16 @@
+const { exec } = require('child_process');
+
+function unsafeExec(input) {
+    // This function executes a command that includes user input without sanitization.
+    const command = `echo ${input}`;
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    });
+}
+
+unsafeExec(process.argv[2]);
